b20df91980dec512a5f4b808a3ece75e7ff875d0
castboy <482819895@qq.com>
2018-01-09 10:10:39 +0800

    Dynamic SQL is not allowed in stored function or trigger trigger里面调用了含有动态sql语句的存储过程

--- sum.sql
+++ sum.sql
@@ -1,20 +1,30 @@
 DROP PROCEDURE IF EXISTS `procedure_netflowip`;
 DELIMITER $
-CREATE PROCEDURE procedure_netflowip(IN interval INT, IN dst_talbe VARCHAR(15))
+CREATE PROCEDURE procedure_netflowip(IN cycle INT, IN dst_table VARCHAR(15), IN new_id INT)
 BEGIN
-    DECLARE insert_time, insert_flow INT
-    DECLARE insert_assetIp VARCHAR(16)
+    DECLARE insert_time, insert_flow INT;
+    DECLARE insert_assetIp VARCHAR(16);
 
-    DECLARE sum_time, old_flow INT
+    DECLARE sum_time, old_flow INT;
 
-    SELECT time, flow, assetIp INTO insert_time, insert_flow, insert_assetIp FROM `netflowip` WHERE id = NEW.id;
-    SET sum_time = ceil(insert_time / interval) * interval
-    SELECT flow into old_flow FROM `dst_talbe` WHERE time = sum_time AND assetIp = insert_assetIp;
+    SET @dst_tbl = dst_table;
+
+    SELECT time, flow, assetIp INTO insert_time, insert_flow, insert_assetIp FROM `netflowip` WHERE id = new_id;
+    SET sum_time = ceil(insert_time / cycle) * cycle;
+
+    PREPARE get_flow FROM "SELECT flow into old_flow FROM ? WHERE time = sum_time AND assetIp = insert_assetIp";
+    EXECUTE  get_flow USING @dst_tbl;
+    DEALLOCATE PREPARE get_flow;
+
     IF old_flow IS NOT NULL THEN
-        UPDATE `dst_talbe` SET flow = insert_flow + old_flow WHERE time = sum_time AND assetIp = insert_assetIp;
+        PREPARE update_flow FROM "UPDATE ? SET flow = insert_flow + old_flow WHERE time = sum_time AND assetIp = insert_assetIp";
+        EXECUTE update_flow USING @dst_tbl;
+        DEALLOCATE PREPARE update_flow;
     ELSE
-        INSERT INTO `dst_talbe` (time, flow, assetIp) VALUES (sum_time, insert_flow, insert_assetIp)
-    END IF
+        PREPARE insert_flow FROM "INSERT INTO ? (time, flow, assetIp) VALUES (sum_time, insert_flow, insert_assetIp)";
+        EXECUTE insert_flow USING @dst_tbl;
+        DEALLOCATE PREPARE insert_flow;
+    END IF;
 END
 $
 DELIMITER ;
@@ -24,7 +34,7 @@ DROP TRIGGER IF EXISTS `trigger_netflowip`;
 DELIMITER $
 CREATE TRIGGER `trigger_netflowip` AFTER INSERT ON `netflowip` FOR EACH ROW
 BEGIN
-    CALL procedure_netflow(900, "netflowip_q")
+    CALL procedure_netflowip(900, "netflowip_q", NEW.id);
 END
 $
 DELIMITER ;
